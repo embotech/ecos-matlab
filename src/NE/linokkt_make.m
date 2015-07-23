4b920fbce703a182fa77a1544383333e9ca2ccef
Lino Widmer <lino@localhost.localdomain>
2015-07-23 09:59:30 +0200

    changed mex-makefile to compile with new cholmod settings

--- src/NE/linokkt_make.m
+++ src/NE/linokkt_make.m
@@ -5,6 +5,6 @@ d = '' ;
 if (~isempty (strfind (computer, '64')))
     d = '-largeArrayDims' ;
 end
-eval (sprintf ('mex %s -I../../ecos/include -I../../ecos/external/CHOLMOD/Include -I../../ecos/AMD/Include -I../../ecos/external/SuiteSparse_config -L../../ecos/external/CHOLMOD/Lib -L../../ecos/external/AMD/Lib -L../../ecos/external/COLAMD/Lib -L../../ecos/external/SuiteSparse_config -lcholmod -lamd -lcolamd -lsuitesparseconfig -lm -lgoto2 -llapack linokkt_mex.c ../../ecos/src/lino_kkt.c ../../ecos/src/cone.c ../../ecos/src/splamm.c ../../ecos/src/spla.c ../../ecos/src/timer.c', d));
+eval (sprintf ('mex %s -I../../ecos/include -I../../ecos/external/CHOLMOD/Include -I../../ecos/amd/Include -I../../ecos/external/SuiteSparse_config -L../../ecos/external/CHOLMOD/Lib -L../../ecos/external/amd -L../../ecos/external/COLAMD/Lib -L../../ecos/external/SuiteSparse_config -lcholmod -lamd -lcolamd -lsuitesparseconfig -lm -lgoto2 -llapack linokkt_mex.c ../../ecos/src/lino_kkt.c ../../ecos/src/cone.c ../../ecos/src/splamm.c ../../ecos/src/spla.c ../../ecos/src/timer.c', d));
 fprintf('linokkt_mex successfully compiled.\n');
 
